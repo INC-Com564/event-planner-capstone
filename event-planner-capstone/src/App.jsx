dc763dc93ca36cfcbea977c4cd0b9894adf9d2a5
INC-Com739 <Iamisaiahcleveland@gmail.com>
2025-11-10 13:37:49 -0600

    API, Node, React, & Tailwind Setup

--- event-planner-capstone/src/App.jsx
+++ event-planner-capstone/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <h1>Event Calendar</h1>
       <div className="card">
         <img src="https://gdoc.io/uploads/printable-monthly-calendar-free-google-docs-template-t-1712x1239.webp" />
-        <button className='Enter'> Enter Date</button>
+       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Enter Date</button>
         <button className='Delete'>Delete Date</button>
       </div>
       <p className="read-the-docs">
